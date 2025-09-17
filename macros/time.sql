6d6687319f1048deeb96601372b21bd6efcb1c4d
mcgrath-dylan <mcgrath.fintech@gmail.com>
2025-09-17 15:33:27 -0700

    fix: harden ntz_hour conversions

--- macros/time.sql
+++ macros/time.sql
@@ -1,3 +1,11 @@
 {% macro ntz_hour(ts) -%}
-  date_trunc('hour', ({{ ts }})::timestamp_ntz)
+  date_trunc(
+    'hour',
+    coalesce(
+      try_to_timestamp_ntz({{ ts }}),
+      try_to_timestamp_ltz({{ ts }})::timestamp_ntz,
+      try_to_timestamp_tz({{ ts }})::timestamp_ntz,
+      ({{ ts }})::timestamp_ntz
+    )
+  )
 {%- endmacro %}
