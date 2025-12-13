d32a26b223cfb39d90af6c065f2e21f10d736cd5
mcgrath-dylan <mcgrath-dylan@users.noreply.github.com>
2025-12-13 05:17:43 +0000

    deploy: 6c4fc8217628a836dff2c2d018a2116c4e6f7906

--- base/compiled/snowflake_finops/models/marts/dim_department.sql
+++
@@ -1,6 +0,0 @@
-
-
-select
-    upper(trim(warehouse_name)) as warehouse_name,
-    trim(department) as department
-from DM_AE_FINOPS_DB.STG.department_mapping
--- base/compiled/snowflake_finops/models/monitors/monitor_freshness_check.sql
+++
@@ -1,4 +0,0 @@
-
-
-select max(hour_end) as last_end_time
-from DM_AE_FINOPS_DB.STG.stg_warehouse_metering
